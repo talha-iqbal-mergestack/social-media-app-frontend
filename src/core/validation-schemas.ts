d2f7af48c8e8fc7a8a13625d7bdf2ec92762669a
Talha Iqbal <talha.iqbal@mergestack.com>
2025-05-16 10:46:33 +0500

    Improved form validation

--- src/core/validation-schemas.ts
+++ src/core/validation-schemas.ts
@@ -5,37 +5,60 @@ import {
 	INVALID_USERNAME_ERROR,
 	NON_SIMILAR_PASSWORDS_ERROR,
 	INVALID_OTP_ERROR,
+	NO_SPACES_IN_PASSWORD_ERROR,
+	INVALID_POST_CONTENT_ERROR,
+	POST_CONTENT_MAX_LENGTH_ERROR,
+	INVALID_OTP_TYPE_ERROR,
+	INVALID_USERNAME_CHARACTERS_ERROR,
+	INVALID_EMAIL_ERROR,
 } from '@/core/constants'
 
 export const signinSchema = object({
-	email: string().email(),
-	password: string().min(8, INVALID_PASSWORD_ERROR),
+	email: string().email(INVALID_EMAIL_ERROR),
+	password: string()
+		.regex(/^[^\s]+$/, NO_SPACES_IN_PASSWORD_ERROR)
+		.min(8, INVALID_PASSWORD_ERROR),
 })
 
 export const signupSchema = object({
-	email: string().email(),
-	name: string().min(1, INVALID_USERNAME_ERROR),
-	password: string().min(8, INVALID_PASSWORD_ERROR),
-	confirmPassword: string().min(8, INVALID_PASSWORD_ERROR),
+	email: string().email(INVALID_EMAIL_ERROR),
+	name: string()
+		.regex(
+			/^(?=.*[a-zA-Z])[a-zA-Z0-9_\s-]+$/,
+			INVALID_USERNAME_CHARACTERS_ERROR
+		)
+		.min(1, INVALID_USERNAME_ERROR),
+	password: string()
+		.regex(/^[^\s]+$/, NO_SPACES_IN_PASSWORD_ERROR)
+		.min(8, INVALID_PASSWORD_ERROR),
+	confirmPassword: string()
+		.regex(/^[^\s]+$/, NO_SPACES_IN_PASSWORD_ERROR)
+		.min(8, INVALID_PASSWORD_ERROR),
 }).refine(data => data.password === data.confirmPassword, {
 	message: NON_SIMILAR_PASSWORDS_ERROR,
 	path: ['confirmPassword'],
 })
 
 export const resetPasswordSchema = object({
-	email: string().email(),
+	email: string().email(INVALID_EMAIL_ERROR),
 })
 
 export const emailVerificationSchema = object({
-	email: string().email(),
-	code: string().length(4, INVALID_OTP_ERROR),
+	email: string().email(INVALID_EMAIL_ERROR),
+	code: string()
+		.regex(/^\d{4}$/, INVALID_OTP_TYPE_ERROR)
+		.min(4, INVALID_OTP_ERROR),
 })
 
 export const resetPasswordVerificationSchema = object({
-	email: string().email(),
-	code: string().length(4, INVALID_OTP_ERROR),
-	password: string().min(8, INVALID_PASSWORD_ERROR),
-	confirmPassword: string().min(8, INVALID_PASSWORD_ERROR),
+	email: string().email(INVALID_EMAIL_ERROR),
+	code: string().regex(/^\d{4}$/, INVALID_OTP_ERROR),
+	password: string()
+		.regex(/^[^\s]+$/, NO_SPACES_IN_PASSWORD_ERROR)
+		.min(8, INVALID_PASSWORD_ERROR),
+	confirmPassword: string()
+		.regex(/^[^\s]+$/, NO_SPACES_IN_PASSWORD_ERROR)
+		.min(8, INVALID_PASSWORD_ERROR),
 }).refine(data => data.password === data.confirmPassword, {
 	message: NON_SIMILAR_PASSWORDS_ERROR,
 	path: ['confirmPassword'],
@@ -43,6 +66,6 @@ export const resetPasswordVerificationSchema = object({
 
 export const postSchema = object({
 	text: string()
-		.min(1, 'Post content is required')
-		.max(500, 'Post content must be less than 500 characters'),
+		.min(1, INVALID_POST_CONTENT_ERROR)
+		.max(500, POST_CONTENT_MAX_LENGTH_ERROR),
 })
